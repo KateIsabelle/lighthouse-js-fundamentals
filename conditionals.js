07212f2ca98533c593048d2cac743725cf6c905b
KateIsabelle <kate.davis887@gmail.com>
2021-01-20 23:05:04 +0000

    Add more from first examples missed

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,28 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!")
+
+///
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+///
+
 const temperature = -1;
 
 if(temperature < 0) {
@@ -18,9 +43,13 @@ if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
 
+/// 
+
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a good idea...");
 }
+
+///
 
 if (!raining) {
   console.log("Leave your umbrella at home!");
